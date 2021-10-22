0c6cdd076e40fc30b7c877e892497ee203cbee7c
TbIKoBKa <zhenya.mormul@gmail.com>
2021-10-22 22:01:05 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Delivery />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Delivery from './components/Delivery.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Delivery
   }
 }
 </script>
